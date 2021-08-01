9d950518b80dde3ea8abf0ff84828ffeb6742d52
max <max@formation.jpa>
2021-08-01 20:54:06 +0200

    202. equals() & hashcode()

--- src/main/java/com/hibernate4all/tutorial/domain/Movie.java
+++ src/main/java/com/hibernate4all/tutorial/domain/Movie.java
@@ -1,5 +1,7 @@
 package com.hibernate4all.tutorial.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -50,6 +52,34 @@ public class Movie {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(31);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if ((!(obj instanceof Movie))) {
+			return false;
+		}
+		Movie other = (Movie) obj;
+		if (id == null && other.getId() == null) {
+			return Objects.equals(name, other.getName())
+					&& Objects.equals(description, other.getDescription())
+					&& Objects.equals(certification, other.getCertification());
+		}
+		return id != null && Objects.equals(id, other.getId());
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", name=" + name + ", description="
+				+ description + ", certification=" + certification + "]";
 	}
 
 }
